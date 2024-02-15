d84fd47d2f9f292ae7ec88593833f5ca1c402472
MeineWFG <Meine.professional@proton.me>
2024-02-15 23:34:16 +0100

    Version 1.10 - Notification live Twitch

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
 const cron = require('node-cron');
-const { cronJSON } = require('../json/config.json');
+const { cronJSON, serverJSON } = require('../json/config.json');
 const { Events } = require('discord.js');
+const { getStreamData } = require('../functions/twitch/getStreamData.js');
 const { updateToken } = require('../functions/twitch/updateToken.js');
 
 module.exports = {
@@ -12,9 +13,12 @@ module.exports = {
 		console.log(`Prêt ! Connecté en tant que ${client.user.tag}`);
 
 		//Tâche Cron
+		cron.schedule(cronJSON.getStreamData, () => {
+			getStreamData(client.channels.cache.get(serverJSON.channel.announcement));
+		})
+
 		cron.schedule(cronJSON.updateToken, () => {
 			updateToken();
 		})
-
 	},
 };
---
+++ functions/twitch/getStreamData.js
@@ -0,0 +1,67 @@
+const axios = require('axios');
+const { EmbedBuilder } = require('discord.js');
+const { format } = require('date-fns');
+const { fr } = require('date-fns/locale');
+const fs = require('fs');
+const { imagesJSON, serverJSON, twitchJSON } = require('../../json/config.json');
+
+
+module.exports = {
+    async getStreamData (announcementChannel) {
+        //Lecture du fichier config.json
+        var configJSON = JSON.parse(fs.readFileSync('./json/config.json'));
+
+        //Récupération de la clé API twitch
+        axios.get(`https://api.twitch.tv/helix/streams?user_login=${twitchJSON.userLogin}`, { headers: { 'Client-Id': twitchJSON.clientID, 'Authorization': `Bearer ${configJSON.twitchJSON.token}` }})
+        .then((response) => {
+            if(response.data.data.length == 0) return;
+            
+            const data = response.data.data[0];
+            var isNewAnnoucement = false;
+
+            if(configJSON.twitchJSON.startedAt != data.started_at){
+                isNewAnnoucement = true;
+                configJSON.twitchJSON.startedAt = data.started_at;
+            }
+
+            if(configJSON.twitchJSON.gameName != data.game_name){
+                isNewAnnoucement = true;
+                configJSON.twitchJSON.gameName = data.game_name;
+            }
+
+            if(isNewAnnoucement){
+                //Réecriture du fichier config.json avec format
+                fs.writeFileSync('./json/config.json', JSON.stringify(configJSON, null, 2));
+                
+                const randomImage = imagesJSON.stream[Math.floor(Math.random() * imagesJSON.stream.length)];
+                const urlThumbnail = data.thumbnail_url.replace('{width}', '1920').replace('{height}', '1080');
+
+                const embed = new EmbedBuilder()
+                    .setColor(`#${Math.floor(Math.random()*16777215).toString(16)}`)
+                    .setTitle(data.title)
+                    .setURL('https://www.twitch.tv/' + data.user_login)
+                    .setAuthor({ name: data.user_name + ' est en direct !', iconURL: `attachment://${imagesJSON.stream_icon}`})
+                    .setThumbnail(`attachment://${randomImage}`)
+                    .setImage(urlThumbnail)
+                    .setDescription(':arrow_forward: https://www.twitch.tv/' + data.user_login)
+                    .addFields(
+                        { name: 'Joue à : ', value: data.game_name, inline: true},
+                        { name: 'Date : ', value: format(new Date(), "EEEE, dd MMMM yyyy", { locale: fr }), inline: true}
+                    )
+                    .setTimestamp()
+
+                announcementChannel.send({ 
+                    content: `** ${serverJSON.text.stream[Math.floor(Math.random() * serverJSON.text.stream.length)]}   @everyone **`, 
+                    embeds: [embed], 
+                    files: [
+                        `./images/${imagesJSON.stream_icon}`, 
+                        `./images/${randomImage}`
+                    ] 
+                })
+            }
+        })
+        .catch(function (error){
+            console.log(error);
+        })
+    }
+}
