abbbf69d92f98aa07e69d52cb95ee2b3a4a6ca16
MeineWFG <Meine.professional@proton.me>
2025-04-07 14:55:31 +0200

    Ajout flux RSS hltv

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,7 @@ const { cronJSON, serverJSON } = require('../json/config.json');
 const { Events } = require('discord.js');
 const { notifyLive } = require('../functions/twitch/notifyLive.js');
 const { updateToken } = require('../functions/twitch/updateToken.js');
+const { getRss } = require('../functions/hltv/getRss.js');
 
 module.exports = {
 	name: Events.ClientReady,
@@ -13,12 +14,20 @@ module.exports = {
 		console.log(`Prêt ! Connecté en tant que ${client.user.tag}`);
 
 		//Tâche Cron
+
+		//Notifie live twitch
 		cron.schedule(cronJSON.notifyLive, () => {
 			notifyLive(client.channels.cache.get(serverJSON.channel.announcement));
 		})
 
+		//Update token twitch
 		cron.schedule(cronJSON.updateToken, () => {
 			updateToken();
+		})
+
+		//Rss HLTV
+		cron.schedule(cronJSON.getRss, () => {
+			getRss(client.channels.cache.get(serverJSON.channel.hltv));
 		})
 	},
 };
